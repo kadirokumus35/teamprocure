e75c4c218f1ccd4b8fe5af6ce20c22e476fd32ea
kadir <kdrokumus35@gmail.com>
2023-02-04 02:46:26 +0300

    test

--- src/test/java/pages/TeamProcure.java
+++ src/test/java/pages/TeamProcure.java
@@ -14,37 +14,74 @@ public class TeamProcure {
     @FindBy(xpath = "//*[text()='Sign-up']")
     public WebElement signUp;
 
-    @FindBy(xpath = "//*[text()='Sign-up']")
-    public WebElement signUp;
+    @FindBy(xpath = "//input[@id='email']")
+    public WebElement eMail;
 
-    @FindBy(xpath = "//*[text()='Sign-up']")
-    public WebElement signUp;
+    @FindBy(xpath = "//input[@id='password']")
+    public WebElement password;
 
-    @FindBy(xpath = "//*[text()='Sign-up']")
-    public WebElement signUp;
+    @FindBy(xpath = "//input[@name='password_confirmation']")
+    public WebElement passwordConfirmation;
 
-    @FindBy(xpath = "//*[text()='Sign-up']")
-    public WebElement signUp;
+    @FindBy(xpath = "//input[@type='checkbox']")
+    public WebElement checkbox;
 
-    @FindBy(xpath = "//*[text()='Sign-up']")
-    public WebElement signUp;
+    @FindBy(xpath = "//div[@class='form-group d-grid mt-4']")
+    public WebElement register;
 
-    @FindBy(xpath = "//*[text()='Sign-up']")
-    public WebElement signUp;
+    @FindBy(xpath = "//*[@id='name']")
+    public WebElement name;
 
-    @FindBy(xpath = "//*[text()='Sign-up']")
-    public WebElement signUp;
+    @FindBy(xpath = "//*[@id='companyName']")
+    public WebElement companyName;
 
-    @FindBy(xpath = "//*[text()='Sign-up']")
-    public WebElement signUp;
+    @FindBy(xpath = "//*[@id='phoneNumber']")
+    public WebElement phoneNumber;
 
-    @FindBy(xpath = "//*[text()='Sign-up']")
-    public WebElement signUp;
+    @FindBy(xpath = "//*[@class='select2-selection__arrow']")
+    public WebElement peopleOfCompany;
 
-    @FindBy(xpath = "//*[text()='Sign-up']")
-    public WebElement signUp;
+    @FindBy(xpath = "//*[@id='title']")
+    public WebElement title;
 
-    @FindBy(xpath = "//*[text()='Sign-up']")
-    public WebElement signUp;
+    @FindBy(xpath = "//div[@class='setup-buttons']")
+    public WebElement next;
+
+    @FindBy(xpath = "(//div[@class='icon-info mt-4 d-flex justify-content-center'])[1]")
+    public WebElement requestManagement;
+
+    @FindBy(xpath = "(//div[@class='icon-info mt-4 d-flex justify-content-center'])[2]")
+    public WebElement procurement;
+
+    @FindBy(xpath = "(//div[@class='icon-info mt-4 d-flex justify-content-center'])[3]")
+    public WebElement rfq;
+
+    @FindBy(xpath = "(//div[@class='icon-info mt-4 d-flex justify-content-center'])[4]")
+    public WebElement warehouse;
+
+    @FindBy (xpath = "//*[@class='btn btn-next btn-primary']")
+    public WebElement next2;
+
+    @FindBy (xpath = "(//*[@class=\"d-flex justify-content-center font-16\"])[1]")
+    public WebElement usStandarts;
+
+    @FindBy (xpath = "(//*[@class=\"d-flex justify-content-center font-16\"])[2]")
+    public WebElement europanStandarts;
+
+    @FindBy (xpath = "//*[text()='Complete']")
+    public WebElement complete;
+
+    @FindBy (xpath = "//*[@class='btn-google']")
+    public WebElement loginWithGoogle;
+
+    @FindBy (xpath = "//*[@id='identifierId']")
+    public WebElement loginEmail;
+
+    @FindBy (xpath = "//*[text()='İleri']")
+    public WebElement ileri;
+
+
+
+
 
 }
--- src/test/java/tests/Sign_Up_.java
+++ src/test/java/tests/Sign_Up_.java
@@ -1,5 +1,11 @@
 package tests;
 
+import com.github.javafaker.Faker;
+import com.github.javafaker.PhoneNumber;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.TeamProcure;
 import utilities.ConfigReader;
@@ -10,11 +16,70 @@ public class Sign_Up_ {
    TeamProcure teamProcure=new TeamProcure();
 
 
+
    @Test
-    public void test01(){
+    public void test01() throws InterruptedException {
         Driver.getDriver().get(ConfigReader.getProperty("teamProcureUrl"));
 
       teamProcure.signUp.click();
+
+      //teamProcure.eMail.sendKeys(ConfigReader.getProperty("eMail"));
+
+       Faker faker=new Faker();
+     String fakeMail=faker.internet().emailAddress();
+     teamProcure.eMail.sendKeys(fakeMail);
+
+      teamProcure.password.sendKeys(ConfigReader.getProperty("password"));
+
+      teamProcure.passwordConfirmation.sendKeys(ConfigReader.getProperty("password"));
+
+      teamProcure.checkbox.click();
+      Thread.sleep(2000);
+
+      teamProcure.register.click();
+      Thread.sleep(2000);
+
+
+      teamProcure.name.sendKeys(ConfigReader.getProperty("name"));
+
+      teamProcure.companyName.sendKeys(ConfigReader.getProperty("company"));
+
+      //teamProcure.phoneNumber.sendKeys(ConfigReader.getProperty("phone"));
+       teamProcure.phoneNumber.sendKeys(faker.phoneNumber().cellPhone());
+
+       WebElement ddm=Driver.getDriver().findElement(By.xpath("//*[@id='companySize']"));
+       Select select=new Select(ddm);
+       select.selectByValue("6-25");
+
+
+       teamProcure.title.sendKeys(ConfigReader.getProperty("title"));
+
+       teamProcure.next.click();
+
+       teamProcure.requestManagement.click();
+
+       teamProcure.rfq.click();
+       Thread.sleep(2000);
+
+       teamProcure.next2.click();
+       Thread.sleep(2000);
+
+
+
+       teamProcure.europanStandarts.click();
+
+       teamProcure.next2.click();
+       Thread.sleep(2000);
+
+
+
+       teamProcure.complete.click();
+       Thread.sleep(2000);
+
+       String expectedTitle="Dashboard - TeamProcure";
+       String actualTitle=Driver.getDriver().getTitle();
+       Assert.assertTrue(actualTitle.contains(expectedTitle));
+
 
     }
 
