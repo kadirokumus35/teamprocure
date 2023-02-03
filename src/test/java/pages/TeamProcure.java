380921d94409ff6592c8edbcadd67c52be9e1aa5
kadir <kdrokumus35@gmail.com>
2023-02-03 23:52:16 +0300

    location

--- src/test/java/pages/TeamProcure.java
+++ src/test/java/pages/TeamProcure.java
@@ -1,4 +1,50 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
 public class TeamProcure {
+
+    public TeamProcure(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+    @FindBy(xpath = "//*[text()='Sign-up']")
+    public WebElement signUp;
+
+    @FindBy(xpath = "//*[text()='Sign-up']")
+    public WebElement signUp;
+
+    @FindBy(xpath = "//*[text()='Sign-up']")
+    public WebElement signUp;
+
+    @FindBy(xpath = "//*[text()='Sign-up']")
+    public WebElement signUp;
+
+    @FindBy(xpath = "//*[text()='Sign-up']")
+    public WebElement signUp;
+
+    @FindBy(xpath = "//*[text()='Sign-up']")
+    public WebElement signUp;
+
+    @FindBy(xpath = "//*[text()='Sign-up']")
+    public WebElement signUp;
+
+    @FindBy(xpath = "//*[text()='Sign-up']")
+    public WebElement signUp;
+
+    @FindBy(xpath = "//*[text()='Sign-up']")
+    public WebElement signUp;
+
+    @FindBy(xpath = "//*[text()='Sign-up']")
+    public WebElement signUp;
+
+    @FindBy(xpath = "//*[text()='Sign-up']")
+    public WebElement signUp;
+
+    @FindBy(xpath = "//*[text()='Sign-up']")
+    public WebElement signUp;
+
 }
--- src/test/java/tests/Sign_Up_.java
+++ src/test/java/tests/Sign_Up_.java
@@ -1,4 +1,23 @@
 package tests;
 
+import org.testng.annotations.Test;
+import pages.TeamProcure;
+import utilities.ConfigReader;
+import utilities.Driver;
+
 public class Sign_Up_ {
+
+   TeamProcure teamProcure=new TeamProcure();
+
+
+   @Test
+    public void test01(){
+        Driver.getDriver().get(ConfigReader.getProperty("teamProcureUrl"));
+
+      teamProcure.signUp.click();
+
+    }
+
+
+
 }
